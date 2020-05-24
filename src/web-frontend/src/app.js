09dc39c91ce73e0c9642cb7ef47724046077e4ee
Jens Stigaard <jensgs@bitbucket.org>
2020-05-24 18:22:51 +0200

    Minor changes

--- src/web-frontend/src/app.js
+++ src/web-frontend/src/app.js
@@ -14,6 +14,7 @@ Vue.use(
 		reconnection: true, // (Boolean) whether to reconnect automatically (false)
 		reconnectionAttempts: 5, // (Number) number of reconnection attempts before giving up (Infinity),
 		reconnectionDelay: 3000, // (Number) how long to initially wait before attempting a new (1000)
+		format: 'json'
 	}
 )
 
